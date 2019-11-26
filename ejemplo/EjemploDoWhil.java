3e80a4430a8fc96cf1c16bb33f3b21f2b32858ed
Kaan <alinurkaanbaytan@hotmail.com>
2019-11-26 16:38:05 +0100

    Primer commit

---
+++ ejemplo/EjemploDoWhil.java
@@ -0,0 +1,12 @@
+package ejemplo;
+
+public class EjemploDoWhil {
+	  public static void main(String[] args) {
+		    int i = 1;
+		    
+		    do {
+		      System.out.println(i);
+		      i++;
+		    } while (i < 11);
+		  }
+}
---
+++ ejemplo/EjemploFo.java
@@ -0,0 +1,9 @@
+package ejemplo;
+
+public class EjemploFo {
+	  public static void main(String[] args) {
+		    for (int i = 1; i < 11; i++) {
+		      System.out.println(i);
+		    }
+		  }
+}
